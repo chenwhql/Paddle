a148ea224d7068a54a4ce7f964e4be9f2c43a214
JiabinYang <marsyang199376@gmail.com>
2021-02-03 05:07:33 +0000

    debug CustomTensor core

--- paddle/fluid/framework/custom_operator.cc
+++ paddle/fluid/framework/custom_operator.cc
@@ -102,6 +102,7 @@ static void RunKernelFunc(const framework::ExecutionContext& ctx,
           "Custom operator can only hold 1 output as vector<Tensor>."));
   auto true_outs = ctx.MultiOutput<Tensor>(out_name[0]);
   for (size_t i = 0; i < true_outs.size(); ++i) {
+      outs.at(i).SetLoD(std::vector<std::vector<size_t>>());
       outs.at(i).ShareDataWith((true_outs)[i]);
   }
 }
